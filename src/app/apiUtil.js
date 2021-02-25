fde414465f4f6447b375a779a16526458f3507a4
KT123 <krishnateja@monocept.com>
2021-02-26 00:58:16 +0530

    prod base URL

--- src/app/apiUtil.js
+++ src/app/apiUtil.js
@@ -3,7 +3,8 @@ import axios from "axios";
 console.log("base url for machine", process.env.BASE_URL);
 let endpoint = axios.create({
  // baseURL: process.env.BASE_URL || "http://localhost:3000",
-  baseURL: "http://localhost:3005",
+  //baseURL: "http://localhost:3005",
+  baseURL: "http://http://13.233.66.63/api",
   withCredentials: true
 });
 
