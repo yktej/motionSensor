9016d8ba585d140c5ccc07bcd1a03c47301fe184
KT123 <krishnateja@monocept.com>
2021-02-26 01:03:37 +0530

    baseURL

--- src/app/apiUtil.js
+++ src/app/apiUtil.js
@@ -4,7 +4,7 @@ console.log("base url for machine", process.env.BASE_URL);
 let endpoint = axios.create({
  // baseURL: process.env.BASE_URL || "http://localhost:3000",
   //baseURL: "http://localhost:3005",
-  baseURL: "http://http://13.233.66.63/api",
+  baseURL: "http://13.233.66.63/api",
   withCredentials: true
 });
 
